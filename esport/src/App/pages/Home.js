763bb6c3f2c6cff29f5e6f9825e19a2588d13d97
HEYITSGILBERT780 <gilbertaragon7801@gmail.com>
2018-09-13 05:23:41 +0000

    Created initial home page

---
+++ esport/src/App/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Esports Player Hub</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
